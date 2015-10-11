a08edc24e739d0d70f0e4b4224bd3116d8ca70c1
NoSunlightForDayz <NoSunlightForDayz@gmail.com>
2015-10-11 18:13:39 -0500

    Update bounce.js

--- js/bounce.js
+++ js/bounce.js
@@ -1,34 +1,43 @@
-var vx = 3;
-var vy = 2;
+$(document).ready(function(){
+    animateDiv();
+    
+});
 
-function hitLR(el, bounding) {
-    if (el.offsetLeft <= 0 && vx < 0) {
-        console.log('LEFT');
-        vx = -1 * vx;
-    }
-    if ((el.offsetLeft + el.offsetWidth) >= bounding.offsetWidth) {
-        console.log('RIGHT');
-        vx = -1 * vx;
-    }
-    if (el.offsetTop <= 0 && vy < 0) {
-        console.log('TOP');
-        vy = -1 * vy;
-    }
-    if ((el.offsetTop + el.offsetHeight) >= bounding.offsetHeight) {
-        console.log('BOTTOM');
-        vy = -1 * vy;
-    }
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
 }
 
-function mover(el, bounding) {
-    hitLR(el, bounding);
-    el.style.left = el.offsetLeft + vx + 'px';
-    el.style.top = el.offsetTop + vy + 'px';
-    setTimeout(function() {
-        mover(el, bounding);
-    }, 50);
-}
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $('.a').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('.a').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+    
+};
 
-setTimeout(function() {
-    mover($('.bouncer')[0], $('gradient')[0]);
-}, 50);
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
